6dc01838c68ee3a33516de43a3f0a5d6c6277482
Lemuel de Sousa <lemuel124.ldsp@gmail.com>
2023-12-04 20:37:04 +0000

    feat: create controller

---
+++ src/main/java/com/github/lemuelsousa/cashcard/CashCardController.java
@@ -0,0 +1,30 @@
+package com.github.lemuelsousa.cashcard;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long id) {
+        Optional<CashCard> cashCard = cashCardRepository.findById(id);
+        if (cashCard.isPresent())
+            return ResponseEntity.ok(cashCard.get());
+
+        return ResponseEntity.notFound().build();
+    }
+
+}
