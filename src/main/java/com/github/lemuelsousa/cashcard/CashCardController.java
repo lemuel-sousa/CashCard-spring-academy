581ada378f9c5354e3eb6edc9fee1109fcdd1d17
Lemuel de Sousa <lemuel124.ldsp@gmail.com>
2023-12-04 21:40:34 +0000

    feat: create cash card post endpoint

--- src/main/java/com/github/lemuelsousa/cashcard/CashCardController.java
+++ src/main/java/com/github/lemuelsousa/cashcard/CashCardController.java
@@ -1,12 +1,16 @@
 package com.github.lemuelsousa.cashcard;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -25,6 +29,16 @@ public class CashCardController {
             return ResponseEntity.ok(cashCard.get());
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> create(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder uri) {
+        CashCard cashCardSaved = cashCardRepository.save(newCashCardRequest);;
+        URI locationOfNewCashCard = uri
+            .path("/cashcards/{id}")    
+            .buildAndExpand(cashCardSaved.id())
+            .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 
 }
