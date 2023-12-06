0d46e90d99475a59f05e9c9ad54a47e743de2bd3
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-12-06 06:29:06 -0300

    feat: congifure web security with basic auth

---
+++ src/main/java/com/github/lemuelsousa/cashcard/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.github.lemuelsousa.cashcard;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(request -> request
+                .requestMatchers("/cashcards/**")
+                // .permitAll());
+                .authenticated())
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder();
+        UserDetails lemuk = users
+            .username("lemuk")
+            .password(passwordEncoder.encode("lemuk123"))
+            .roles()
+            .build();
+        
+        return new InMemoryUserDetailsManager(lemuk);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
