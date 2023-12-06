825d36e968224ec98e123a8da62e780907f80c5d
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-12-06 06:31:17 -0300

    feat: add "owner" attribute for cash casrds

--- src/main/java/com/github/lemuelsousa/cashcard/CashCard.java
+++ src/main/java/com/github/lemuelsousa/cashcard/CashCard.java
@@ -2,6 +2,4 @@ package com.github.lemuelsousa.cashcard;
 
 import org.springframework.data.annotation.Id;
 
-public record CashCard(@Id Long id, Double amount) {
-
-}
+public record CashCard(@Id Long id, Double amount, String owner) {}
